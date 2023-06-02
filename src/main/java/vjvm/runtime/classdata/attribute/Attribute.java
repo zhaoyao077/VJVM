a9e47e2b16e1582f57637df72fef6c659ca19823
zhaoyao <201870139@smail.nju.edu.cn>
2023-06-02 12:23:06 +0800

    lab2

---
+++ src/main/java/vjvm/runtime/classdata/attribute/Attribute.java
@@ -0,0 +1,19 @@
+package vjvm.runtime.classdata.attribute;
+
+import lombok.var;
+import lombok.SneakyThrows;
+import vjvm.runtime.classdata.ConstantPool;
+
+import java.io.DataInput;
+
+public abstract class Attribute {
+
+  @SneakyThrows
+  public static Attribute constructFromData(DataInput input, ConstantPool constantPool) {
+    var nameIndex = input.readUnsignedShort();
+    var attrLength = Integer.toUnsignedLong(input.readInt());
+
+    // TODO: detect and construct Code attribute
+    return new UnknownAttribute(input, attrLength);
+  }
+}
